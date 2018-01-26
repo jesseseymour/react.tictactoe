fcf42b31ed206292126c0c676d27f23e672b884a
jesseseymour <jesse.seymour@catapultmarketing.com>
2018-01-26 15:47:11 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return(
+      <div>This is my app</div>
+    )
+  }
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { render } from 'react-dom'
+import App from './App'
+
+window.React = React
+
+let _render = () => {
+  const App = require('./App').default
+  render(
+    <App />,
+    document.getElementById('react-container')
+  )
+}
+
+if (module.hot) {
+  const renderApp = _render;
+  _render = () => {
+    try {
+      renderApp()
+    }
+    catch(error) {
+      console.log(error)
+    }
+  }
+
+  module.hot.accept("./App.js", () => {
+    setTimeout(_render)
+  })
+}
+
+_render()
